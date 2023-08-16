d9f43fc0268118ac4443f6e716a5b857367c1b4e
PirAdnan1 <109432989+PirAdnan1@users.noreply.github.com>
2023-08-16 01:02:06 -0700

    fix profile page

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -7,12 +7,12 @@ import Container from "@/components/Container";
 
 function Layout({ children }) {
   return (
-    <div className="mx-36">
+    <div className="md:mx-36 mx:4">
       <Navigation />
       <Container>
-        <div className="flex flex-col md:flex-row space-x-20 mt-16 max-w-[628px] w-full">
+        <div className="flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-20 space-x-4 mt-16 max-w-[628px] w-full">
           <SideBar />
-          <div className="w-[888px]">{children}</div>
+          <div className="mx-4 md:mx-0">{children}</div>
         </div>
       </Container>
     </div>
